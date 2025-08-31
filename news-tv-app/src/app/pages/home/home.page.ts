b0ffbbfe81b88dce7d162e0bd2eefd8ee8ddf474
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-08-31 13:04:52 -0500

    implementacion de metodos de la pagina home

--- news-tv-app/src/app/pages/home/home.page.ts
+++ news-tv-app/src/app/pages/home/home.page.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NewsService } from 'src/app/services/news.service';  
-
-
+import { ToastService } from 'src/app/providers/toast';
+import { NavController } from '@ionic/angular';
+import { StorageService } from 'src/app/providers/storage';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,7 +10,36 @@ import { NewsService } from 'src/app/services/news.service';
   standalone: false,
 })
 export class HomePage {
+articles: any[] = [];
+selectedCategory: string = 'general';
+categories: string[] = ['general', 'business', 'entertainment', 'health', 'science', 'sports', 'technology'];
 
-  constructor() {}
 
+
+  constructor(
+private newsService: NewsService,
+private toast: ToastService,
+private navCtrl: NavController,
+private storage: StorageService
+  ) {}
+
+ ngOnInit(){
+
+ }
+
+ loadNews(){
+this.newsService.getTopHeadlines(this.selectedCategory).subscribe({
+next: res => this.articles = res.articles,
+error: () => this.toast.show('Error al cargar noticias', {color: 'danger'})
+});
+ }
+changeCategory(cat: string) {
+this.selectedCategory = cat;
+this.loadNews();
+}
+
+logout() {
+this.storage.remove('current_user');
+this.navCtrl.navigateRoot('/login');
+}
 }
